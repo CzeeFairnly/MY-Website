f0c4c48330300263de3000e54c0220d248323637
CzeeFairnly <crfa1610@gmail.com>
2026-09-26 02:13:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById("tombol-masuk").addEventListener("click", function () {
-  document.getElementById("halaman-awal").style.display = "none";
-  document.getElementById("halaman-utama").classList.remove("tersembunyi");
-});
+
