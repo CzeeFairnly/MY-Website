eab0a5874cd6e2788166222c72c10c16acc51713
CzeeFairnly <crfa1610@gmail.com>
2026-09-26 01:51:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("tombol-masuk").addEventListener("click", function () {
+  document.getElementById("halaman-awal").style.display = "none";
+  document.getElementById("halaman-utama").classList.remove("tersembunyi");
+});
